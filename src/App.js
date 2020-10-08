aa59f0d46fd15bd54b7c3a4930a51465ae5c3490
Dylan Cooper <dylancooper155@gmail.com>
2020-10-08 15:13:46 -0400

    pulling in data, css needs fixed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import RandomUser from "./components/RandomUser";
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    userData: [],
+   
+  };
+
+  loadData = async () => {
+    const response = await fetch("https://randomuser.me/api/?results=10");
+    const data = await response.json();
+    return data;
+};
+
+  handleCLick = async () => {
+    const userData = await this.loadData();
+    
+    this.setState({
+      userData: userData.results,
+    });
+  };
+
+
+  async componentDidMount() {
+    const userData = await this.loadData();
+   
+    this.setState({
+      userData: userData.results
+    });
+  }
+  
+  render() {
+    const { userData } = this.state;
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
+        <h1>Random User!</h1>
       </header>
+      <button onClick={this.handleClick}>Load more users</button>
+      {this.state.userData.length ? (
+      <RandomUser userData={userData} />
+      ) : (
+      <p>No User Data Loaded</p>
+      )}
     </div>
-  );
+  ); 
 }
-
+}
 export default App;
